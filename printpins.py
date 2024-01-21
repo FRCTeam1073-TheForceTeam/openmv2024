da42314e26a740cb494b81aa02e477fee5400745
SnowballGus <82126044+SnowballGus@users.noreply.github.com>
2024-01-21 16:12:04 -0500

    Update printpins.py
    Sargent: cleaned up, comments

--- printpins.py
+++ printpins.py
@@ -18,19 +18,23 @@ import machine
 # example see the BLE-Shield driver.
 # uart = UART(3, 19200, bits=8, parity=0, stop=1, timeout_char=200)
 
-uart = UART(3, 921600, bits=8, parity=0, stop=1, timeout_char=2000)
+uart = UART(3, 921600, bits=8, parity=0, stop=1, timeout_char=2000)  # default (200) was too low
 tx_pin = machine.Pin("P3", Pin.OUT)
 tx_pin.value(0)# "active high" 1 means enable transmit
-rx_pin = machine.Pin("P2", Pin.OUT) # recieve enable is active high
+rx_pin = machine.Pin("P2", Pin.OUT)
 rx_pin.value(0) # "active low" 0 means enable read.
-configs = 'config vals'`
+configs = 'config vals'
 
 print('about to loop')
 while True:
-    #print('looping')
-    #print(f'any: {uart.any()}')
-    #output = uart.write('imopenmv')
-    output = uart.read(1)
-    #print(f'type: {type(input)}')
+    output = uart.read(1)  # ".read()" by itself doesn't work, there's number of bytes, timeout, etc.
     print(output)
     time.sleep_ms(21)
+
+# next steps:
+# objective: read from java AND reply to java e.g. read q (for "query") and return config values
+# write something to handle tx/rx
+#    PRO TIP: it's easy to make mistakes fiddling with little numbers. do it right once in a function or w/e and use that instead.
+# do actual vision!
+# start creating/developing skeleton
+# in java, build something general for other robot code to use. like, "SerialComms.gamepiece() or whatever, I dunno, you guys figure it out.
