b5d81109986ab70711c0d9cdcc92b931df9eb4c8
SnowballGus <82126044+SnowballGus@users.noreply.github.com>
2024-01-21 15:47:45 -0500

    Sargent: it wooooorks

--- printpins.py
+++ printpins.py
@@ -12,24 +12,25 @@ import time
 from pyb import UART
 from machine import Pin
 import machine
-from pyb import UART
 
 # Always pass UART 3 for the UART number for your OpenMV Cam.
 # The second argument is the UART baud rate. For a more advanced UART control
 # example see the BLE-Shield driver.
 # uart = UART(3, 19200, bits=8, parity=0, stop=1, timeout_char=200)
 
-uart = UART(3, 19200, bits=8, parity=0, stop=1, timeout_char=200)
+uart = UART(3, 921600, bits=8, parity=0, stop=1, timeout_char=2000)
 tx_pin = machine.Pin("P3", Pin.OUT)
-rx_pin = machine.Pin("P2", Pin.OUT)
-tx_pin.value(1)
-rx_pin.value(0)
-configs = 'config vals'
+tx_pin.value(0)# "active high" 1 means enable transmit
+rx_pin = machine.Pin("P2", Pin.OUT) # recieve enable is active high
+rx_pin.value(0) # "active low" 0 means enable read.
+configs = 'config vals'`
 
 print('about to loop')
 while True:
     #print('looping')
     #print(f'any: {uart.any()}')
-    input = UART.read(uart)
+    #output = uart.write('imopenmv')
+    output = uart.read(1)
     #print(f'type: {type(input)}')
-    print(input)
+    print(output)
+    time.sleep_ms(21)
