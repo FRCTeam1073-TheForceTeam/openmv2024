e2a4789e5c2373a9b15a3180a658e2225b3f1af7
William Van Uitert <82126044+SnowballGus@users.noreply.github.com>
2024-01-24 20:58:55 -0500

    created skeleton for pin_control
    created skeleton for classes and function of the camera and recieving messages
    
    Co-Authored-By: C0leSc0ttB0t <107157923+C0leSc0ttB0t@users.noreply.github.com>
    Co-Authored-By: NateSargent <6923903+NateSargent@users.noreply.github.com>

--- pin_control.py
+++ pin_control.py
@@ -7,23 +7,43 @@ sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time = 2000)
 
+#tx_pin = machine.Pin("P3", Pin.OUT)s
+#rx_pin = machine.Pin("P2", Pin.OUT)
+
 clock = time.clock()
 
-Class GetPins(id, baut_rate, pin1, pin2, dataBits, serialPortParity, stopBits):
+Class GetPins(id, baud_rate, pin1, pin2, dataBits, serialPortParity, stopBits):
     def __init__(self):
-        name = self.id
-        rate = self.baut_rate
-        pin2 = self.pin1
-        pin3 = self.pin2
-        bits = self.dataBits
-        parity = self.serialPortParity
-        stop = self.stopBits
+        self.id = name
+        self.baud_rate = rate
+        self.tx_pin = pin1
+        self.rx_pin = pin2
+        self.dataBits = bits
+        self.serialPortParity = parity
+        self.stopBits = stop
+
+        self.tx_pin.value(0)# "active high" 1 means enable transmit
+        self.rx_pin.value(0) # "active low" 0 means enable read.
+
 
     def transmit():
         pass
 
     def recieve():
-        pass
+
+        msg = []
+
+        while True:
+            output = uart.read(1)  # ".read()" by itself doesn't work, there's number of bytes, timeout, etc.
+            msg.append(output)
+            if output ==  '\n':
+              break
+        print(output)
+
+        if(output[0] != self.id):
+            return
+        else:
+            pass
 
 
 Class Camera(id):
@@ -40,6 +60,11 @@ Class Camera(id):
     def FindGamePiece():
         pass
 
+cam = Camera(1, yavtastuff)
+while True:
+    msg = waitformsg()
+    if msg[1] == 'A':
+      # do april tag stuff
 
 while(True):
 
